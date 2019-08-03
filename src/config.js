f947db9144b6cd846923eb164a8943915e067bae
Callie Porter-Borden <porterhyphenborden@gmail.com>
2019-08-03 13:33:29 -0400

    updated datalist inputs for safari compatibility

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    //API_ENDPOINT: `https://agile-lake-91659.herokuapp.com`,
-    API_ENDPOINT:  'http://localhost:8000',
+    API_ENDPOINT: `https://agile-lake-91659.herokuapp.com`,
+    //API_ENDPOINT:  'http://localhost:8000',
     TOKEN_KEY: 'spirited-client-auth-token',
   }
