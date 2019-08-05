3bca2d440b0065cc8c98a2e2e0dc1b75f5a379aa
Callie Porter-Borden <porterhyphenborden@gmail.com>
2019-08-05 18:23:01 -0400

    added demo user

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -8,6 +8,10 @@ export default class LandingPage extends Component {
             <div className='welcome'>
                 <h2>Welcome to Spirited!</h2>
                 <p>Spirited is a tool for the modern bartender and craft cocktail enthusiast. Search our curated selection of cocktail recipes, including both classics and modern favorites. Register for an account to create a personal cocktail library, add your own recipes, and update those recipes as you make changes.</p>
+                <p>Please use this demo user account to try out the app:<br />
+                Username: DemoUser<br />
+                Password: @DemoUser1
+                </p>
             </div>
         )
     }
